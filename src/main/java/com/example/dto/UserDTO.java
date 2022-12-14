ff8ecce81a860ce11411d7cc4ab1cf5f8be38558
MuratOldman <muratkara15102@gmail.com>
2022-12-14 18:23:40 -0500

    Initial commit

---
+++ src/main/java/com/example/dto/UserDTO.java
@@ -0,0 +1,30 @@
+package com.example.dto;
+
+
+import com.example.enums.Gender;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String passWord;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String confirmPassWord;
+    private boolean enabled;
+    private String phone;
+    private RoleDTO role;
+    private Gender gender;
+
+}
+
+
